122476f506f980feabc1b68bd28a14e7904b75a4
Erik <erik.ra@gmail.com>
2017-04-24 16:16:36 +0200

    Add project files.

---
+++ Ladder/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ladder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputs = Console.ReadLine().Split(' ');
+
+            double a = double.Parse(inputs[0]);
+            double v = double.Parse(inputs[1]);            
+
+            Console.WriteLine(Math.Ceiling((a / Math.Tan(v * (Math.PI / 180))) / Math.Cos(v * (Math.PI / 180))));
+        }
+    }
+}
